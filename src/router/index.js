e55f23debb4c1ab70930ead7e13547f94c27d8c6
Yutaro Aratake <yar0316@gmail.com>
2022-02-16 23:17:52 +0900

    add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,50 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import SignIn from '../views/album/Signin.vue'
+import AlbumIndex from '../views/album/Index.vue'
+import AlbumCreate from '../views/album/Create.vue'
+import AlbumEdit from '../views/album/Edit.vue'
+import AlbumShow from '../views/album/Show.vue'
+import PhotoCreate from '../views/photo/Create.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    redirect: { name: 'AlbumIndex' }
+  },
+  {
+    path: '/signin',
+    name: 'SignIn',
+    component: SignIn
+  },
+  {
+    path: '/albums',
+    name: 'AlbumIndex',
+    component: AlbumIndex
+  },
+  {
+    path: '/albums/create',
+    name: 'AlbumCreate',
+    component: AlbumCreate
+  },
+  {
+    path: '/albums/edit',
+    name: 'AlbumEdit',
+    component: AlbumEdit
+  },
+  {
+    path: '/albums/show',
+    name: 'AlbumShow',
+    component: AlbumShow
+  },
+  {
+    path: '/photo/create',
+    name: 'PhotoCreate',
+    component: PhotoCreate
+  }
+]
 
 const router = new VueRouter({
   mode: 'history',
